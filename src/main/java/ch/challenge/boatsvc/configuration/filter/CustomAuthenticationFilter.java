f76798a5a85dff5a306301d2c4a1eeceec2d61d0
omarlo <omar.lo@jpl.ch>
2022-08-23 11:44:23 +0200

    enable token expiration day

--- src/main/java/ch/challenge/boatsvc/configuration/filter/CustomAuthenticationFilter.java
+++ src/main/java/ch/challenge/boatsvc/configuration/filter/CustomAuthenticationFilter.java
@@ -8,7 +8,6 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import javax.servlet.FilterChain;
@@ -57,9 +56,9 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     final String username = userSpring.getUsername();
     String access_token = JWT.create()
         .withSubject(username)
-//        .withExpiresAt(java.sql.Date.valueOf(
-//            LocalDate.now().plusDays(this.jwtPropertiesConfig.getTokenExpirationAfterDays())))
-        .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+        .withExpiresAt(java.sql.Date.valueOf(
+            LocalDate.now().plusDays(this.jwtPropertiesConfig.getTokenExpirationAfterDays())))
+//        .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000)) // For testing
         .withIssuer(request.getRequestURL().toString())
         .withClaim("roles",
             userSpring.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList())
