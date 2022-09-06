473db587b56de7dbcf30b75c8b43fd1ad834dd82
omarlo <omar.lo@jpl.ch>
2022-09-06 20:42:42 +0200

    test boat repository

---
+++ src/test/java/ch/challenge/boatsvc/core/api/boat/BoatRepositoryTest.java
@@ -0,0 +1,24 @@
+package ch.challenge.boatsvc.core.api.boat;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class BoatRepositoryTest {
+
+  @Autowired
+  private BoatRepository boatRepository;
+
+  @Test
+  public void shouldSaveBoat() {// Before to run test comment code CommandLineRunner in main class
+    final BoatEntity expected = new BoatEntity();
+    expected.setName("Boat 1");
+    expected.setDescription("Boat 1 Desc..");
+    final BoatEntity actual = boatRepository.save(expected);
+    assertThat(actual).usingRecursiveComparison().ignoringFields("id").isEqualTo(expected);
+  }
+
+}
